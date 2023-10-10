373ee0de6478eaeb7bfa1dbb513d35d57b9071a1
lavkushdiwedi82 <130441676+lavkushdiwedi82@users.noreply.github.com>
2023-10-10 14:39:51 +0530

    Add files via upload
    query all selector use

--- dom.js
+++ dom.js
@@ -1,20 +1,35 @@
-var items = document.getElementsByClassName('list-group-item'
-);
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'hello 2';
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'yellow';
+// var items = document.getElementsByClassName('list-group-item'
+// );
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'hello 2';
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'yellow';
 
-//3rd element bcg green
-var items = document.getElementsByClassName('list-group-item'
-);
-console.log(items);
-console.log(items[3]);
-items[3].textContent = 'hello 2';
-items[3].style.fontWeight = 'bold';
-items[3].style.backgroundColor = 'green';
+// //3rd element bcg green
+// var items = document.getElementsByClassName('list-group-item'
+// );
+// console.log(items);
+// console.log(items[3]);
+// items[3].textContent = 'hello 2';
+// items[3].style.fontWeight = 'bold';
+// items[3].style.backgroundColor = 'green';
 
-for(var i=0;i<items.length;i++){
-    items[i].style.fontColor = 'bold';
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontColor = 'bold';
+// }
+
+
+// query selector all
+var titles = document.querySelectorAll('.title');
+console.log(titles);
+titles[0].textContent = 'Hello';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+
+
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor = '#f4f4f4';
+    even[i].style.backgroundColor = '#ccc';
 }
